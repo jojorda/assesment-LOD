5a065abbf3e957c816156b87c87692a386f56f0b
jojorda <jojojordan@gmail.com>
2023-11-02 15:53:18 +0700

    foodAjah

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -78,52 +78,55 @@ export default function Page() {
           </Button>
         </section>
         <section className="flex flex-row items-end justify-between z-10 mx-auto">
-          <Button
-            onClick={handlePrev}
-            className="shadow-sm rounded-full text-sm font-bold p-3"
-            style={{ backgroundColor: "#FF922C", color: "white" }}
-          >
-            <BsArrowLeft style={{height:25, width:25}}/>
-          </Button>
-          <Image
-            src={data[rotation].food_image}
-            className='md:w-72 md:h-72 lg:w-80 w-60 h-60 lg:h-80 animate-scale'
-            alt={data[rotation].name}
-            width={100}
-            height={100}
-            style={{
-              marginTop: "50px",
-              transform: `rotate(${imageRotation}deg)`, // Menggunakan imageRotation
-              transition: 'transform 0.5s',
-              width: `${shifted ? 540 : 540}px`,
-              height: `${shifted ? 350 : 350}px`,
-            }}
-          />
-          <Button
-            onClick={handleNext}
-            className="shadow-sm shadow-[#FF922C] rounded-full text-sm font-bold p-3"
-            style={{ backgroundColor: "#FF922C", color: "white" }}
-          >
-            <BsArrowRight style={{height:25, width:25}}/>
-          </Button>
-        </section>
+  <Button
+    onClick={handlePrev}
+    className="shadow-sm rounded-full text-sm font-bold p-3"
+    style={{ backgroundColor: "#FF922C", color: "white", marginLeft: "100px" }}
+  >
+    <BsArrowLeft style={{height:25, width:25}}/>
+  </Button>
+  <Image
+    src={data[rotation].food_image}
+    className='md:w-72 md:h-72 lg:w-80 w-60 h-60 lg:h-80 animate-scale'
+    alt={data[rotation].name}
+    width={100}
+    height={100}
+    style={{
+      marginTop: "50px",
+      transform: `rotate(${imageRotation}deg)`,
+      transition: 'transform 0.5s',
+      width: `${shifted ? 540 : 540}px`,
+      height: `${shifted ? 350 : 350}px`,
+      marginLeft: "100px"
+    }}
+  />
+  <Button
+    onClick={handleNext}
+    className="shadow-sm shadow-[#FF922C] rounded-full text-sm font-bold p-3"
+    style={{ backgroundColor: "#FF922C", color: "white", marginLeft: "90px" }}
+  >
+    <BsArrowRight style={{height:25, width:25}}/>
+  </Button>
+</section>
+
       </div>
       <section
-        className="rounded-bl-full mx-auto lg:mr-auto h-[20rem] w-full md:h-[35rem] lg:h-[35rem] lg:w-[77rem]"
+        className="absolute overflow-hidden rounded-bl-full rounded-br-full mx-auto right-0 top-0 h-[20rem] w-full lg:mr-auto md:h-[32rem] lg:h-[44rem] lg:w-[95rem]"
         style={{
           position: "absolute",
           overflow: "hidden",
-          borderBottomRightRadius: "605rem" ,
+          // borderBottomRightRadius: "605rem" ,
           backgroundColor: "#FFEEDE",
-          right: "0",
-          top: "0",
+          right: "-223px",
+          top: "-200px",
+          // left: "223px"
         }}
       >
         <Image
+          className="w-full md:w-10/12 mx-auto ml-15 transform rotate-0"
           src={data[rotation].cirles}
-          className="w-full md:w-10/12 mx-auto mt-16 transform rotate-0"
           alt={data[rotation].name}
-          style={{
+          style={{ marginTop: '300px', width: '60%',
             transform: `rotate(${rotateValue}deg)`,
             transition: 'transform 0.5s'
           }}
